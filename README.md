2112a7caaa94dae18835d680c9005bc80ef7f55b
dbhujle <bhujle.Deepa@gmail.com>
2019-06-04 23:59:32 -0500

    #update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TheBurgerShop
+
+You can order the burgers.
+Once devouverd they will go to past orders list
+The past order list gets saved which will be diaplyed when you initially load the form
+
+Enjoy the burgers!
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -14,6 +14,7 @@ $(function() {
 
         var id = this.id;
         var lItem = "#"+id;
+        var listItems = $("ul.current").children();
         // console.log(lItem);
         //get the text from the listitem
         var nameOfBurger = $(lItem).text();
@@ -23,55 +24,63 @@ $(function() {
             name: nameOfBurger
         };
 
-        //post request
+        //post request and also add to past list and remove from current
         $.ajax("/api/burgers", {
             type: "POST",
             data: myBurger
             }).then( 
                 function(){                                   
-                //console.log(myBurger);                
+                //console.log(myBurger);                   
                 $(lItem).remove();     
                 $(lItem).remove();   
+                console.log("Length is: " + listItems.length);
+                fillThePastOrder(nameOfBurger);
+                if(listItems.length == 0)
+                {
+                    location.reload();
+                }
             });
-
     });
 });
 
 //add to the Past order (also adds to the database for future)
 function fillThePastOrder(burgerName)
 {
+    var listItems = $("ul.order").children();
+    var id = listItems.length;
     var pastList = $("#pastOrder");
+    var h4 = $("<h4>");
     var row = $("<li>");
-    row.addClass("newOrder");
+    row.addClass("burgerList");
     row.attr("id", id);
     row.append(burgerName);
-    pastList.append(row);
+    h4.append(row);
+    pastList.prepend(h4);
 }
 
 function fillTheOrder(burgerName)
 {
-    var listItems = $("ul.order").children();
+    var listItems = $("ul.current").children();
     var id = listItems.length;
-    console.log("The id is " + id);
+    // console.log("The id is " + id);
     if(id == 0)
     {
         var h3 = $("<h3>");
         h3.addClass("burgerList");
         h3.text("Current Orders:");
-        $("#curOrder").append(h3);
+        $("#curOrderHeader").prepend(h3);
     }
  
     var h4 = $("<h4>");
 
     var row = $("<li>");
-    row.addClass("newOrder");
+    row.addClass("orderNew");
     row.attr("id", id);
     row.append(burgerName);
 
     var span1 = $("<span>");
     span1.addClass("order");
     span1.append(row);
-
     // <span class="a">Aliquam</span> <span class="a"></span>
 
     var btn = $("<button>");
@@ -79,12 +88,12 @@ function fillTheOrder(burgerName)
     btn.attr("id", id);
     btn.text("Devoured!");
 
-    var span2 = $("<span>");
-    span2.addClass("order");
-    span2.append(btn);
+    // var span2 = $("<span>");
+    // span2.addClass("order");
+    span1.append(btn);
 
     h4.append(span1);
-    h4.append(span2);
+    // h4.append(span2);
     // $("#btnOrder").append(btnRow);    
     $("#curOrder").append(h4);
 }
